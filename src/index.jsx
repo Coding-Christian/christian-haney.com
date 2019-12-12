b5b249714496cb3a71239deb17497610b9c12e18
Christian Haney <christianhaney1996@gmail.com>
2019-12-12 14:47:39 -0800

    created basic directory structure and installed dependencies

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CSSTransitionGroup from 'react-transition-group';
+
+const placeHolder = (<h1>This is the personal webpage for web developer Christian Haney. It is a work in progress.</h1>);
+
+ReactDOM.render(placeHolder, document.querySelector('#root'));
