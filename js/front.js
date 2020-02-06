ef858b62ae4d2c182ddbe06cf70dd67f1bd073a3
Christian Haney <christianhaney1996@gmail.com>
2020-02-06 11:36:13 -0800

    removed map and updated hero image

--- js/front.js
+++ js/front.js
@@ -32,63 +32,6 @@ $(function () {
             $('.navbar-collapse').removeClass('show');
             $('.navbar-toggler').attr('aria-expanded', 'false');
         }
-    });
-
-    // ---------------------------------------------------------- //
-    // Map init
-    // ---------------------------------------------------------- //
-
-    map();
+    })
 
 });
-
-// ------------------------------------------------------ //
-// styled Leaflet  Map
-// ------------------------------------------------------ //
-
-function map() {
-
-    var mapId = 'map',
-        mapCenter = [33.7495, -117.8263],
-        mapMarker = true;
-
-    if ($('#' + mapId).length > 0) {
-
-        var icon = L.icon({
-            iconUrl: 'img/marker.png',
-            iconSize: [25, 37.5],
-            popupAnchor: [0, -18],
-            tooltipAnchor: [0, 19]
-        });
-
-        var map = L.map(mapId, {
-            center: mapCenter,
-            zoom: 13,
-            zoomControl: false,
-            doubleClickZoom: false,
-            dragging: false,
-            tap: false,
-            scrollWheelZoom: false
-        });
-
-        var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
-            attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
-        });
-
-        Wikimedia.addTo(map);
-
-        if (mapMarker) {
-            var marker = L.marker(mapCenter, {
-                icon: icon
-            }).addTo(map);
-
-            marker.bindPopup("<div><p class='font-weight-bold'>I live in Tustin, CA and am open to any opportunities in Orange County and South Los Angeles County.</p></div>", {
-                minwidth: 200,
-                maxWidth: 600,
-                className: 'map-custom-popup'
-            })
-
-        }
-    }
-
-}
