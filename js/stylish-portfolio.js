862d844202b359f984fbbfb8ece173a5ce5d333b
Christian Haney <christianhaney1996@gmail.com>
2020-06-05 00:38:35 -0700

    added fade in animations to in progress sections

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -53,6 +53,24 @@
   });
 
   // CUSTOM SCRIPTS //
+  function elementInViewport(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+    while (el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+    return (
+      top >= window.pageYOffset &&
+      left >= window.pageXOffset &&
+      top + height <= window.pageYOffset + window.innerHeight &&
+      left + width <= window.pageXOffset + window.innerWidth
+    );
+  }
+
   $('.btn').on('mouseenter', function (event) {
     if (!$(event.target).hasClass('shine')) {
       console.log('here');
@@ -61,6 +79,19 @@
         $(event.target).toggleClass('shine');
       }, 500);
     }
+  });
+  setTimeout(function () {
+    $('.fade-in').first().toggleClass('is-visible');
+  }, 0);
+  $(document).scroll(function () {
+    this.querySelectorAll('.fade-in').forEach(function (el) {
+      if (elementInViewport(el)) {
+        $(el)
+          .toggleClass('is-visible')
+          .toggleClass('faded')
+          .toggleClass('fade-in');
+      }
+    });
   });
 })(jQuery); // End of use strict
 
