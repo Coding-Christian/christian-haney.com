b641de627ad2e614c555e553f8bb5e0b898f5e41
Christian Haney <christianhaney1996@gmail.com>
2020-06-14 19:08:56 -0700

    added contact links

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,14 @@ function modules() {
   var bootstrap = gulp
     .src('./node_modules/bootstrap/dist/**/*')
     .pipe(gulp.dest('./vendor/bootstrap'));
+  //Font Awesome
+  var fontAwesomeCSS = gulp
+    .src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
+    .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+  // Font Awesome Webfonts
+  var fontAwesomeWebfonts = gulp
+    .src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
   // jQuery Easing
   var jqueryEasing = gulp
     .src('./node_modules/jquery.easing/*.js')
@@ -66,7 +74,13 @@ function modules() {
       '!./node_modules/jquery/dist/core.js'
     ])
     .pipe(gulp.dest('./vendor/jquery'));
-  return merge(bootstrap, jquery, jqueryEasing);
+  return merge(
+    bootstrap,
+    fontAwesomeCSS,
+    fontAwesomeWebfonts,
+    jquery,
+    jqueryEasing
+  );
 }
 
 // CSS task
